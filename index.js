24a2a237ae281e66d504d9f82bcd2a25c9abf7b0
Andreassen <spencer.andreassen@PIONEER.COM>
2018-04-06 15:40:02 -0500

    Adding test cases

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = (input) => {
+
+    return undefined;
+}
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,7 +1,21 @@
 'use strict';
 
+const fs = require('fs');
+const text = fs.readFileSync('./spec/input.txt', 'utf8');
+const index = require('../index');
+
 describe('index', function () {
-    it('should return true', function () {
-        expect(true).toEqual(true);
+    it('should return 10 items', function () {
+        expect(index(text).length).toEqual(10);
+    });
+
+    it('should return items in descending order', function () {
+        let input = 'the the my you';
+        expect(index(input)).toEqual([{ the: 2 }, { my: 1 }, { you: 1 }]);
+    });
+
+    it('should return items in alphabetical order if there is a tie', function () {
+        let input = 'the my you them';
+        expect(index(input)).toEqual([{ my: 1 }, { the: 1 }, { them: 1 }, { you: 1 }]);
     });
 });
