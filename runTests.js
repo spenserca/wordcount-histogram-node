bb6029f87a8de3827b87a045a4dd3b5a79dab6a8
Andreassen <spencer.andreassen@PIONEER.COM>
2018-04-06 07:44:04 -0500

    Setting up tests

--- runTests.js
+++ runTests.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const Jasmine = require('jasmine');
+const reporters = require('jasmine-reporters');
+const JasmineConsoleReporter = require('jasmine-console-reporter');
+const path = require('path');
+
+const jasmine = new Jasmine();
+jasmine.loadConfig({
+    spec_dir: '.',
+    spec_files: [
+        'spec/*.spec.js'
+    ]
+});
+
+var junitReporter = new reporters.JUnitXmlReporter({
+    filePrefix: "TEST-",
+    savePath: path.join(__dirname, 'reports'),
+    consolidateAll: false
+});
+
+jasmine.addReporter(junitReporter);
+
+jasmine.addReporter(new JasmineConsoleReporter({
+    colors: 2,
+    cleanStack: 2
+}));
+
+jasmine.onComplete(function (passed) {
+    if (passed) {
+        process.exit();
+    }
+    else {
+        console.log('At least one spec has failed');
+        process.exit(1);
+    }
+});
+
+jasmine.execute();
---
+++ spec/index.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('index', function () {
+    it('should return true', function () {
+        expect(true).toEqual(true);
+    });
+});
