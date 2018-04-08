23a9cb22b31ba3f1950516254478acbff6c1cab3
Andreassen <spencer.andreassen@PIONEER.COM>
2018-04-08 11:05:46 -0500

    Need to fix sorting by count and then alphabetically in case of ties

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 'use strict';
 
 module.exports = (input) => {
+    const wordCounts = [];
+    const output = input.replace(/[^a-zA-Z\s]/g, ' ')
+        .split(/\s/)
+        .filter(s => s != '')
+        .reduce((acc, cv, ci, array) => {
+            let word = acc.find(el => el.key == cv.toUpperCase());
+            if (!word) {
+                acc.push({ key: cv.toUpperCase(), value: 1 });
+            }
+            else {
+                acc[acc.indexOf(word)].value++;
+            }
 
-    return undefined;
+            return acc;
+        }, [])
+        .sort(greatestToLeastValue)
+        .sort(alphabetically);
+
+    return output.slice(0, 10);
+}
+
+function greatestToLeastValue(a, b) {
+    if (a.value < b.value) {
+        return -1;
+    }
+    if (a.value > b.value) {
+        return 1;
+    }
+    return 0;
+}
+
+function alphabetically(a, b) {
+    if (a.key < b.key) {
+        return -1;
+    }
+    if (a.key > b.key) {
+        return 1;
+    }
+    return 0;
 }
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,26 +1,25 @@
 'use strict';
 
-const fs = require('fs');
-const text = fs.readFileSync('./spec/input.txt', 'utf8');
 const index = require('../index');
 
 describe('index', function () {
     it('should return 10 items', function () {
-        expect(index(text).length).toEqual(10);
+        let input = 'one two three four five six seven eight nine ten eleven';
+        expect(index(input).length).toEqual(10);
     });
 
     it('should return items in descending order', function () {
         let input = 'the the my you';
-        expect(index(input)).toEqual([{ the: 2 }, { my: 1 }, { you: 1 }]);
+        expect(index(input)).toEqual([{ key: 'THE', value: 2 }, { key: 'MY', value: 1 }, { key: 'YOU', value: 1 }]);
     });
 
     it('should return items in alphabetical order if there is a tie', function () {
         let input = 'the my you them';
-        expect(index(input)).toEqual([{ my: 1 }, { the: 1 }, { them: 1 }, { you: 1 }]);
+        expect(index(input)).toEqual([{ key: 'MY', value: 1 }, { key: 'THE', value: 1 }, { key: 'THEM', value: 1 }, { key: 'YOU', value: 1 }]);
     });
 
     it('should remove punctuation from input', function () {
         let input = 'you, my. my; :?\'-()&$!'
-        expect(index(input)).toEqual([{ my: 2 }, { you: 1 }]);
+        expect(index(input)).toEqual([{ key: 'MY', value: 2 }, { key: 'YOU', value: 1 }]);
     });
 });
