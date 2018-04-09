3d28eee8f6ab95ff6cee29ffbef1cf179b48ea32
Andreassen <spencer.andreassen@PIONEER.COM>
2018-04-09 14:55:27 -0500

    Adding error handling

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Application running on ${port}`));
 
-app.use(function (req, res, next) {
-    res.status(404).send("Sorry can't find that!")
+app.use((req, res, next) => {
+    res.status(404).send('Sorry can\'t find that!');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
