ba2ce607c3b68f1a1551d6833d65895944012014
Andreassen <spencer.andreassen@PIONEER.COM>
2018-04-09 14:40:50 -0500

    Adding 404 handling

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ const port = 5000;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Application running on ${port}`));
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+});
