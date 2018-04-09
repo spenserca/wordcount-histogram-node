206459187d7e5cbf21045bd6abc5a9337c0a8d82
Andreassen <spencer.andreassen@PIONEER.COM>
2018-04-09 15:02:21 -0500

    Serving static page from express

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
 
 app.listen(port, () => console.log(`Application running on ${port}`));
 
